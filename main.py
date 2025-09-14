c04e7fcbc05ff5e3c0893ac4873af7e9991f41e1
Dmitry Varchak <dmitvar@gmail.com>
2025-09-14 17:39:38 +0300

    Знакомство с gRPC протоколом

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import json
+
+user_data = {
+    'name': 'Olga',
+    'age': 47,
+    'gender': 'female',
+    'is_student': False
+}
+
+with open("json-exaple.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+    print(data)
+
+with open("json-user.json", "w", encoding="utf-8") as f:
+    json.dump(user_data, f)
+
