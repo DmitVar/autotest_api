ecbadb07f40d81a54f00668dfc2f5869d8378e8c
Dmitry Varchak <dmitvar@gmail.com>
2025-10-12 16:15:13 +0300

    9.1 Рефакторинг и подготовка к написанию API автотестов. Практика реализации фикстур

--- fixtures/courses.py
+++ fixtures/courses.py
@@ -11,16 +11,21 @@ class CourseFixture(BaseModel):
     request: CreateCourseRequestSchema
     response: CreateCourseResponseSchema
 
+
 @pytest.fixture
 def courses_client(function_user: UserFixture) -> CourseClient:
     return get_courses_client(function_user.authentication_user)
 
+
+@pytest.fixture()
 def function_course(
         courses_client: CourseClient,
         function_user: UserFixture,
         function_file: FileFixture
 ) -> CourseFixture:
-    request = CreateCourseRequestSchema()
+    request = CreateCourseRequestSchema(
+        preview_file_id=function_file.response.file.id,
+        created_by_user_id=function_user.response.user.id
+    )
     response = courses_client.create_course(request)
     return CourseFixture(request=request, response=response)
-
---
+++ fixtures/exercises.py
@@ -0,0 +1,22 @@
+from clients.exercises.exercises_client import get_exercises_client, ExercisesClient
+from clients.exercises.exercises_schema import CreateExerciseRequestShema, CreateExerciseResponseShema
+from fixtures.courses import CourseFixture
+from fixtures.users import UserFixture
+
+
+class ExerciseFixture:
+    request: CreateExerciseRequestShema
+    response: CreateExerciseResponseShema
+
+
+def exercises_client(function_user: UserFixture) -> ExercisesClient:
+    return get_exercises_client(function_user.authentication_user)
+
+
+def function_exercises(
+        exercises_client: ExercisesClient,
+        function_course: CourseFixture
+) -> ExerciseFixture:
+    request = CreateExerciseRequestShema(course_id=function_course.response.course.id)
+    response = exercises_client.create_exercise(request)
+    return ExerciseFixture(request=request, response=response)
